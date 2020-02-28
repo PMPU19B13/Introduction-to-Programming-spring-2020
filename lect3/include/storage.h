87937e44af29d281da2d52c1e7b883f7397cdcaf
Daniil <daniil1001k@gmail.com>
2020-02-28 13:35:20 +0200

    Update storage.h

--- lect3/include/storage.h
+++ lect3/include/storage.h
@@ -1,43 +1,56 @@
-#ifndef STORAGE_H_INCLUDED
-#define STORAGE_H_INCLUDED
+#pragma once
 
-//#define T int
+using namespace std;
 
-template<typename T> class Storage {
+template<typename T>
+class Storage {
 private:
-    T *data;
-    size_t size;
+	T* data;
+	size_t size;
 
 public:
-    Storage() {
-        data = nullptr;
-        size = 0;
-    }
+	Storage() {
+		data = nullptr;
+		size = 0;
+	}
 
-    void AddElem(T val) {
-        if (data == nullptr) {
-            data = new T[1];
-            data[0] = val;
-            size = 1;
-        } else {
-            T *buf = new T[size + 1];
-            for (size_t i = 0; i < size; i++) {
-                buf[i] = data[i];
-            }
-            buf[size] = val;
-            delete [] data;
-            data = buf;
-            size++;
-        }
-    };
+	void AddElem(T val) {
+		if (data == nullptr) {
+			data = new T[1];
+			data[0] = val;
+			size = 1;
+		}
+		else {
+			T* buf = new T[size + 1];
+			for (size_t i = 0; i < size; i++) {
+				buf[i] = data[i];
+			}
+			buf[size] = val;
+			delete[] data;
+			data = buf;
+			size++;
+		}
+	};
 
-    T GetElemByNum(size_t num) {
-        return data[num];
-    };
-    size_t TotalSize() {
-        return size;
-    };
+	T GetElemByNum(size_t num) {
+		return data[num];
+	};
+	size_t TotalSize() {
+		return size;
+	};
 
 };
 
-#endif // STORAGE_H_INCLUDED
+bool testStorage4() {
+	Storage<int> storage;
+	for (int k = 0; k < 4; k++) {
+		storage.AddElem(k);
+	}
+	if (storage.TotalSize() != 4) return false;
+	for (int k = 0; k < 4; k++) {
+		if (storage.GetElemByNum(k) != k) {
+			return false;
+		}
+	}
+	return true;
+}
