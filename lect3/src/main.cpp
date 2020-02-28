3431f24f8c92f161239299ca806b3b45db215604
Daniil <daniil1001k@gmail.com>
2020-02-28 13:33:45 +0200

    Update main.cpp

--- lect3/src/main.cpp
+++ lect3/src/main.cpp
@@ -6,17 +6,29 @@ using namespace std;
 
 int main()
 {
+	Storage<double> storage;
+	storage.AddElem(1.1);
+	storage.AddElem(2.2);
+	storage.AddElem(3);
+	storage.AddElem(4);
+	storage.AddElem(5);
 
-    Storage<double> storage;
-    storage.AddElem(4.5);
-    cout << storage.TotalSize() << endl;
+	Storage<int> intStorage;
+	intStorage.AddElem(3);
+	intStorage.AddElem(45);
 
-    Storage<int> intStorage;
-    intStorage.AddElem(3);
-    cout << intStorage.TotalSize() << endl;
+	Storage<Point> pointStorage;
+	pointStorage.AddElem(Point(2.3, 4.7));
+	pointStorage.AddElem(Point(1.3, 12.3));
 
-    Storage<Point> pointStorage;
-    pointStorage.AddElem(Point(2.3, 4.7));
+	Point p1(2, 45.4);
+	Point p2(21.2, 45.1);
 
-    return 0;
+	Segment s1(&p1, &p2);
+
+	Point p = s1.getStart();
+
+	cout << "total size: " << storage.TotalSize() << endl;
+	cout << (testStorage4() ? "CORRECT" : "FAILURE") << endl;
+	return 0;
 }
