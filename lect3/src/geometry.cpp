5ad394fd98b213d72bfe4866c45890a93b910805
Daniil <daniil1001k@gmail.com>
2020-02-28 12:56:04 +0200

    Create geometry.cpp

---
+++ lect3/src/geometry.cpp
@@ -0,0 +1,23 @@
+#include "geometry.h"
+
+Point::Point(double _x, double _y)
+{
+	x = _x;
+	y = _y;
+}
+
+Segment::Segment(Point* st, Point* ed)
+{
+	if ((st != nullptr) && (ed != nullptr)) {
+		start = st;
+		end = ed;
+	}
+	else {
+		throw(Error());
+	}
+}
+
+Point Segment::getStart()
+{
+	return *start;
+}
