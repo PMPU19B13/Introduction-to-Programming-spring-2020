33e702f1c8973389c91a28c1b7c7cd7f5c48b96a
Daniil <daniil1001k@gmail.com>
2020-02-28 13:34:53 +0200

    Update geometry.h

--- lect3/include/geometry.h
+++ lect3/include/geometry.h
@@ -1,56 +1,22 @@
-#ifndef GEOMETRY_H_INCLUDED
-#define GEOMETRY_H_INCLUDED
-
+#pragma once
 #include "error.h"
+
+using namespace std;
 
 class Point
 {
 public:
-    double x;
-    double y;
-    Point(double _x = 0, double _y = 0)
-    {
-        x = _x;
-        y = _y;
-        //cout << "Point was created" << endl;
-    }
+	double x;
+	double y;
+	Point(double _x = 0, double _y = 0);
 };
 
 class Segment
 {
-private:
-    Point *start;
-    Point *end;
-    //bool valid;
-
 public:
-    Segment(Point *st, Point *ed)
-    {
-        if ((st != nullptr) && (ed != nullptr)) {
-            start = st;
-            end = ed;
-            //cout << "Segment was created" << endl;
-            //valid = true;
-        }
-        else {
-            //cout << "Error" << endl;
-            throw(Error());
-            //valid = false;
-            //exit(1);
-        }
-    }
-    //Segment(double x1 = 0, double y1 = 1, double x2 = 0, double y2 = 1)
-    //{
-    //    *start = Point(x1, y1);
-    //    *end = Point(x2, y2);
-    //    cout << "Segment was created" << endl;
-    //}
-
-    //bool is_valid()
-    //{
-    //    return valid;
-    //}
+	Segment(Point* st, Point* ed);
+	Point getStart();
+private:
+	Point* start;
+	Point* end;
 };
-
-
-#endif // GEOMETRY_H_INCLUDED
