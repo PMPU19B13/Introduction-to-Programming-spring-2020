c1a635ca9e767e790f2b6026b95eb46e1c4831b2
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2020-03-02 15:04:48 +0300

    Create Controller.h

---
+++ Project files/include/Controller.h
@@ -0,0 +1,33 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "storage.h"
+#include "geometry.h"
+#include "ID.h"
+
+enum PrimType
+{
+    P_Point, P_Segment, P_Circle
+};
+
+enum PrimType
+{
+    R_Distance, R_Paral, R_Eq, R_PonC
+};
+
+class Controller
+{
+    public:
+        Controller();
+        void addPrimitive(PrimType, Storage<double>);
+        void delPrimitive(ID);
+
+        void addRequirement(ReqType, ID, ID, double *param = nullptr);
+        void delRequirement(ID);
+    private:
+        Storage<Point> m_points;
+        Storage<Segment> m_segments;
+        Storage<Circle> m_circles;
+};
+
+#endif // CONTROLLER_H
