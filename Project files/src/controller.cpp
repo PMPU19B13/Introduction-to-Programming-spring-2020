b9f4dbf6a8f36cb32803afd5029da83736f306f7
Daniil <daniil1001k@gmail.com>
2020-03-03 06:49:59 +0300

    Update and rename Controller.cpp to controller.cpp

---
+++ Project files/src/controller.cpp
@@ -0,0 +1,21 @@
+#include "controller.h"
+
+Controller::Controller()
+{
+
+}
+
+void Controller::addPrimitive(PrimType pt, Storage<double> params)
+{
+    switch (pt)
+    {
+        case P_Point:
+            break;
+
+        case P_Segment:
+            break;
+
+        case P_Circle:
+    }
+
+}
