27b0dd1cd4656cad4b774eaacafc2969e6316e93
Daniil <daniil1001k@gmail.com>
2020-03-03 13:23:27 +0300

    Move implementation out of header file

--- Project files/include/storage.h
+++ Project files/include/storage.h
@@ -1,73 +1,21 @@
 #pragma once
 
-#include <stddef.h>
-
-#ifdef DEBUG
-#include <iostream>
-#endif
+#include <cstddef>
 
 template<typename T>
 class Storage
 {
-private:
-    T *data;
-    size_t size;
-
 public:
-    Storage()
-    {
-        data = nullptr;
-        size = 0;
-#ifdef DEBUG
-        std::cout << "Storage constructor called" << std::endl;
-#endif
-    }
+    Storage();
 
-    void AddElem(T val)
-    {
-        if (data == nullptr)
-        {
-            data = new T[1];
-            data[0] = val;
-            size = 1;
-        }
-        else
-        {
-            T *buf = new T[size + 1];
-            for (size_t i = 0; i < size; i++)
-            {
-                buf[i] = data[i];
-            }
-            buf[size] = val;
-            delete[] data;
-            data = buf;
-            size++;
-        }
-    };
+    void add(T value);
 
-    T GetElemByNum(size_t num)
-    {
-        return data[num];
-    };
+    T get(size_t index);
 
-    size_t TotalSize()
-    {
-        return size;
-    };
+    size_t size();
+
+private:
+    T *m_data;
+    size_t m_size;
 
 };
-/*
-bool testStorage4() {
-	Storage<int> storage;
-	for (int k = 0; k < 4; k++) {
-		storage.AddElem(k);
-	}
-	if (storage.TotalSize() != 4) return false;
-	for (int k = 0; k < 4; k++) {
-		if (storage.GetElemByNum(k) != k) {
-			return false;
-		}
-	}
-	return true;
-}
-*/
