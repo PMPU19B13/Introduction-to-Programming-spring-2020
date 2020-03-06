6b32e69d703953ecda80a3373058eb27cad3c17e
Daniil <daniil1001k@gmail.com>
2020-03-06 09:23:55 +0300

    Update controller.cpp

--- Project files/src/controller.cpp
+++ Project files/src/controller.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
+#include <drawer.h>
 
 #include "controller.h"
 
-Controller::Controller()
-{
-
-}
+Controller::Controller() = default;
 
 void Controller::addPrimitive(PrimitiveType type, Storage<double> params)
 {
     switch (type)
     {
         case P_Point:
+            if (params.size() == 2)
+                m_points.add(Point(params.get(0), params.get(1)));
+            else
+                throw BadArg();
             break;
 
         case P_Segment:
+            if (params.size() == 4)
+            {
+                m_points.add(Point(params.get(0), params.get(1)));
+                Point start = m_points.get(m_points.size() - 1);
+                m_points.add(Point(params.get(2), params.get(3)));
+                Point end = m_points.get(m_points.size() - 1);
+
+                m_segments.add(Segment(&start, &end));
+            }
+            else
+                throw BadArg();
             break;
 
         case P_Circle:
+            if (params.size() == 3)
+            {
+                m_points.add(Point(params.get(0), params.get(1)));
+                Point center = m_points.get(m_points.size() - 1);
+
+                m_circles.add(Circle(&center, params.get(2)));
+            }
+            else
+                throw BadArg();
             break;
 
         default:
@@ -30,11 +52,17 @@ void Controller::addPrimitive(PrimitiveType type, Storage<double> params)
 
 void Controller::updateView()
 {
-//        Drawer drawer;
-//        for (point in m_points)
-//            drawer.drawPrimitive(point);
-//        for (segment in m_segments)
-//            drawer.drawPrimitive(segment);
-//        for (circle in m_circles)
-//            drawer.drawPrimitive(circle);
+    Drawer drawer;
+    for (int i = 0; i < m_points.size(); ++i)
+    {
+//        drawer.drawPrimitive(m_points.get(i));
+    }
+    for (int i = 0; i < m_segments.size(); ++i)
+    {
+//        drawer.drawPrimitive(m_segments.get(i));
+    }
+    for (int i = 0; i < m_circles.size(); ++i)
+    {
+//        drawer.drawPrimitive(m_circles.get(i));
+    }
 }
