a826580a68e4381141b181ae1d30baf86262a2a9
Daniil <daniil1001k@gmail.com>
2020-03-06 09:23:28 +0300

    Update geometry.cpp

--- Project files/src/geometry.cpp
+++ Project files/src/geometry.cpp
@@ -6,21 +6,27 @@ Point::Point(double x, double y)
     m_y = y;
 }
 
-void Point::setX(double x) {
+void Point::setX(double x)
+{
     m_x = x;
 }
 
-void Point::setY(double y) {
+void Point::setY(double y)
+{
     m_y = y;
 }
 
-double Point::getX() {
+double Point::getX()
+{
     return m_x;
 }
 
-double Point::getY() {
+double Point::getY()
+{
     return m_y;
 }
+
+Segment::Segment() = default;
 
 Segment::Segment(Point *start, Point *end)
 {
@@ -31,7 +37,7 @@ Segment::Segment(Point *start, Point *end)
     }
     else
     {
-        throw (Error());
+        throw Error();
     }
 }
 
@@ -40,7 +46,17 @@ Point Segment::getStart()
     return *m_start;
 }
 
+Circle::Circle() = default;
+
 Circle::Circle(Point *center, double radius)
 {
-
+    if (center != nullptr)
+    {
+        m_center = center;
+        m_radius = radius;
+    }
+    else
+    {
+        throw Error();
+    }
 }
