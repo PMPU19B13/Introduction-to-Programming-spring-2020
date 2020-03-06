bc0c19d60209f9cfce9ee2d3d16acdb0846a3d00
Daniil <daniil1001k@gmail.com>
2020-03-06 11:03:03 +0300

    Update main.cpp

--- Project files/src/main.cpp
+++ Project files/src/main.cpp
@@ -7,29 +7,52 @@
 #include "ID.h"
 #include "controller.h"
 
+void printContents(Storage<double> s)
+{
+    for (size_t k = 0; k < s.TotalSize(); ++k)
+        std::cout << s.GetElemByNum(k) << std::endl;
+}
+
+
 int main()
 {
+//    int a = 123456;
+//    int &ra = a;
+//    int *pa = &a;
+
+
+//    std::cout << a << std::endl;
+//    std::cout << ra << std::endl;
+//    std::cout << *pa << std::endl;
+
     try
     {
-        Controller c;
-        Storage<double> s;
-        s.AddElem(0);
-        s.AddElem(1);
-        c.addPrimitive(P_Point, s);
+        while (true)
+        {
+            Storage<double> s;
+            s.AddElem(0);
+            s.AddElem(1);
+            printContents(s);
+        }
+//        Controller c;
+//            c.addPrimitive(P_Point, s);
 
+//            s.AddElem(1);
+//            s.AddElem(4);
+//
         Storage<double> d;
         d.AddElem(0);
         d.AddElem(1);
         d.AddElem(4);
         d.AddElem(5);
 
-        c.addPrimitive(P_Segment, d);
-
-        c.updateView();
+        printContents(d);
+//            c.addPrimitive(P_Segment, d);
+//            c.updateView();
 
     } catch (Error)
     {
-        
+
     } catch (BadArg)
     {
         std::cout << "Whoops!" << std::endl;
