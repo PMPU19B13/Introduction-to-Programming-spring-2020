cced1768716442d3bd83e414588decc051c31e5c
Victor <61326288+kolbacer@users.noreply.github.com>
2020-03-15 23:37:10 +0300

    Add files via upload

--- Project files/include/drawer.h
+++ Project files/include/drawer.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "controller.h"
+#include "fstream"
 
 class Drawer
 {
 public:
     void drawPrimitive(PrimitiveType type, Storage<double> params);
+	void drawPrimitive(std::fstream& file, PrimitiveType type, Storage<double> params);
 };
