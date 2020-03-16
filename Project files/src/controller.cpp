22a22e6739c91d00054d00ea2adc63c861b50fee
Daniil Kostitsin <daniil1001k@gmail.com>
2020-03-16 13:36:45 +0300

    Implement writePrimitive and readPrimitive

--- Project files/src/controller.cpp
+++ Project files/src/controller.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstddef>
+#include <fstream>
 
 #include "controller.h"
 #include "error.h"
@@ -139,4 +140,124 @@ void Controller::removePrimitive(const ID& id)
     }
 
     // Закончить реализацию
+}
+
+void Controller::readPrimitive(const std::string& fileName)
+{
+    std::ifstream file;
+
+    file.open(fileName);
+
+    if (!file.is_open())
+    {
+        return;
+    }
+
+    std::string typeName;
+
+    while (!file.eof())
+    {
+        file >> typeName;
+
+        if (typeName == "Point")
+        {
+            std::string temp;
+            double x;
+            double y;
+
+            // Point { x 12.9 y 16.5 }
+            file >> temp >> temp >> x >> temp >> y >> temp;
+
+            Storage<double> args;
+            args.add(x);
+            args.add(y);
+
+            addPrimitive(P_Point, args);
+        }
+        else if (typeName == "Segment")
+        {
+            std::string temp;
+            double x1;
+            double y1;
+            double x2;
+            double y2;
+
+            // Segment { start { x 15 y 10 } end { x 16 y 17 } }
+            file >> temp >> temp >> temp >> temp >> x1 >> temp >> y1
+                 >> temp >> temp >> temp >> temp >> x2 >> temp >> y2 >> temp >> temp;
+
+            Storage<double> args;
+            args.add(x1);
+            args.add(y1);
+            args.add(x2);
+            args.add(y2);
+
+            addPrimitive(P_Segment, args);
+        }
+        else if (typeName == "Circle")
+        {
+            std::string temp;
+            double xCenter;
+            double yCenter;
+            double radius;
+
+            // Circle { center { x 16 y 17 } radius 4 }
+            file >> temp >> temp >> temp >> temp >> xCenter >> temp >> yCenter
+                 >> temp >> temp >> radius >> temp;
+
+            Storage<double> args;
+            args.add(xCenter);
+            args.add(yCenter);
+            args.add(radius);
+
+            addPrimitive(P_Circle, args);
+        }
+        else
+        {
+            file.close();
+            throw Error();
+        }
+    }
+
+    file.close();
+}
+
+void Controller::writePrimitive(const std::string& fileName)
+{
+    std::ofstream file;
+
+    file.open(fileName);
+
+    if (!file.is_open())
+    {
+        return;
+    }
+
+    List<Pair<ID, Point>>::Marker pointMarker = m_points.createMarker();
+    while (pointMarker.isValid())
+    {
+        Storage<double> args = pointMarker.getValue().value.getParams();
+        file << "Point { x " << args[0] << " y " << args[1] << " }" << std::endl;
+        pointMarker.next();
+    }
+
+    List<Pair<ID, Segment>>::Marker segmentMarker = m_segments.createMarker();
+    while (segmentMarker.isValid())
+    {
+        Storage<double> args = segmentMarker.getValue().value.getParams();
+        file << "Segment { start { x " << args[0] << " y " << args[1] << " } end { x "
+             << args[2] << " y " << args[3] << " } }" << std::endl;
+        segmentMarker.next();
+    }
+
+    List<Pair<ID, Circle>>::Marker circleMarker = m_circles.createMarker();
+    while (circleMarker.isValid())
+    {
+        Storage<double> args = circleMarker.getValue().value.getParams();
+        file << "Circle { center { x " << args[0] << " y " << args[1] << " } radius "
+             << args[2] << " }" << std::endl;
+        circleMarker.next();
+    }
+
+    file.close();
 }
