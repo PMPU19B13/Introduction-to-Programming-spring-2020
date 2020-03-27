757a17000e1f24134f721304ada169cdc3d40edf
Grigorev Grigorii <55593191+GrigorevGrigorii@users.noreply.github.com>
2020-03-27 17:23:22 +0300

    Update requirement.h

--- Project files/include/requirement.h
+++ Project files/include/requirement.h
@@ -11,14 +11,9 @@ public:
 class HorizontalRequirement
 {
 public:
-	HorizontalRequirement(Segment* segment) :m_segment(segment)
-	{
-	}
+	HorizontalRequirement(Segment* segment);
 
-	virtual double error() const
-	{
-		return std::abs(m_segment->getEnd().getY() - m_segment->getStart().getY());
-	}
+	virtual double error() const;
 
 private:
 	Segment* m_segment;
@@ -27,14 +22,9 @@ private:
 class VerticalRequirement
 {
 public:
-	VerticalRequirement(Segment* segment) :m_segment(segment)
-	{
-	}
+	VerticalRequirement(Segment* segment);
 
-	virtual double error() const
-	{
-		return std::abs(m_segment->getEnd().getX() - m_segment->getStart().getX());
-	}
+	virtual double error() const;
 
 private:
 	Segment* m_segment;
@@ -43,16 +33,39 @@ private:
 class PointOnSegmentRequirement
 {
 public:
-	PointOnSegmentRequirement(Point* point, Segment* segment) :m_point(point), m_segment(segment) 
-	{
-	}
+	PointOnSegmentRequirement(Point* point, Segment* segment);
 
-	virtual double error() const
-	{
-		return std::abs(m_point->getX() - m_segment->getEnd().getX()) / (m_point->getX() - m_segment->getStart().getX());
-	}
+	virtual double error() const;
 
 private:
 	Segment* m_segment;
 	Point* m_point;
 };
+
+
+HorizontalRequirement::HorizontalRequirement(Segment* segment) : m_segment(segment)
+{
+}
+
+double HorizontalRequirement::error() const
+{
+	return std::abs(m_segment->getEnd().getY() - m_segment->getStart().getY());
+}
+
+VerticalRequirement::VerticalRequirement(Segment* segment) : m_segment(segment)
+{
+}
+
+double VerticalRequirement::error() const
+{
+	return std::abs(m_segment->getEnd().getX() - m_segment->getStart().getX());
+}
+
+PointOnSegmentRequirement::PointOnSegmentRequirement(Point* point, Segment* segment) : m_point(point), m_segment(segment)
+{
+}
+
+double PointOnSegmentRequirement::error() const
+{
+	return std::abs(m_point->getX() - m_segment->getEnd().getX()) / (m_point->getX() - m_segment->getStart().getX());
+}
