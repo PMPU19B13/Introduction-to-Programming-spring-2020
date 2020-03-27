78c6b068d4a1c39862b808999ae15348adf3fdae
Grigorev Grigorii <55593191+GrigorevGrigorii@users.noreply.github.com>
2020-03-27 07:42:42 +0300

    Update id.h

--- Project files/include/id.h
+++ Project files/include/id.h
@@ -15,10 +15,6 @@ public:
 
     static size_t getLastCount();
 
-    static void initCount(size_t newCount);
-
-    static size_t getLastCount();
-
 private:
     Storage<unsigned char> m_array;
     static size_t m_count;
