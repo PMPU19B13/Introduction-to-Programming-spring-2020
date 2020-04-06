1636278dcd816d326d26eaafabba9897a02fc8cc
Daniil Kostitsin <daniil1001k@gmail.com>
2020-04-06 09:46:29 +0300

    Change spaces to tabs in src

--- Project files/src/drawer.cpp
+++ Project files/src/drawer.cpp
@@ -4,57 +4,57 @@
 
 void Drawer::drawPrimitive(PrimitiveType type, const Storage<double>& params)
 {
-    switch (type)
-    {
-    case P_Point:
-    {
-        if (params.size() != 2)
-            throw BadArgument();
+	switch (type)
+	{
+	case P_Point:
+	{
+		if (params.size() != 2)
+			throw BadArgument();
 
-        sf::CircleShape shape(100.f);
-        shape.setPosition(worldToPx(sf::Vector2f(params[0], params[1])));
-        shape.setRadius(2);
-        shape.setFillColor(sf::Color::Green);
-        shape.setOrigin(shape.getRadius(), shape.getRadius());
-        window.draw(shape);
+		sf::CircleShape shape(100.f);
+		shape.setPosition(worldToPx(sf::Vector2f(params[0], params[1])));
+		shape.setRadius(2);
+		shape.setFillColor(sf::Color::Green);
+		shape.setOrigin(shape.getRadius(), shape.getRadius());
+		window.draw(shape);
 
-        break;
-    }
-    case P_Segment:
-    {
-        if (params.size() != 4)
-            throw BadArgument();
+		break;
+	}
+	case P_Segment:
+	{
+		if (params.size() != 4)
+			throw BadArgument();
 
-        sf::VertexArray shape(sf::LinesStrip, 2);
+		sf::VertexArray shape(sf::LinesStrip, 2);
 
-        shape[0].color = sf::Color::White;
-        shape[0].position = worldToPx(sf::Vector2f(params[0], params[1]));
-        shape[1].position = worldToPx(sf::Vector2f(params[2], params[3]));
-        shape[1].color = sf::Color::White;
+		shape[0].color = sf::Color::White;
+		shape[0].position = worldToPx(sf::Vector2f(params[0], params[1]));
+		shape[1].position = worldToPx(sf::Vector2f(params[2], params[3]));
+		shape[1].color = sf::Color::White;
 
-        window.draw(shape);
+		window.draw(shape);
 
-        break;
-    }
-    case P_Circle:
-    {
-        if (params.size() != 3)
-            throw BadArgument();
+		break;
+	}
+	case P_Circle:
+	{
+		if (params.size() != 3)
+			throw BadArgument();
 
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color(0, 0, 0, 0));
-        shape.setPosition(worldToPx(sf::Vector2f(params[0], params[1])));
-        shape.setRadius(params[2]);
-        shape.setOutlineColor(sf::Color::Green);
-        shape.setOutlineThickness(1);
-        shape.setOrigin(shape.getRadius(), shape.getRadius());
-        window.draw(shape);
+		sf::CircleShape shape(100.f);
+		shape.setFillColor(sf::Color(0, 0, 0, 0));
+		shape.setPosition(worldToPx(sf::Vector2f(params[0], params[1])));
+		shape.setRadius(params[2]);
+		shape.setOutlineColor(sf::Color::Green);
+		shape.setOutlineThickness(1);
+		shape.setOrigin(shape.getRadius(), shape.getRadius());
+		window.draw(shape);
 
-        break;
-    }
-    default:
-        std::cerr << "No implementation found!" << std::endl;
-        throw BadArgument();
-    }
+		break;
+	}
+	default:
+		std::cerr << "No implementation found!" << std::endl;
+		throw BadArgument();
+	}
 
 }
--- Project files/src/main.cpp
+++ Project files/src/main.cpp
@@ -9,184 +9,184 @@
 class WithName
 {
 public:
-    virtual const char* getName() const = 0;
+	virtual const char* getName() const = 0;
 };
 
 class Person : public WithName
 {
 public:
-    Person(const char* name, int height) : m_name(name), m_height(height)
-    {
-    }
+	Person(const char* name, int height) : m_name(name), m_height(height)
+	{
+	}
 
-    virtual const char* getName() const
-    {
-        return m_name;
-    }
+	virtual const char* getName() const
+	{
+		return m_name;
+	}
 
-    int getHeight() const
-    {
-        return m_height;
-    }
+	int getHeight() const
+	{
+		return m_height;
+	}
 
 private:
-    const char* m_name;
-    int m_height;
+	const char* m_name;
+	int m_height;
 };
 
 class Student : public Person
 {
 public:
-    Student() : Person("John", 160)
-    {
-    }
+	Student() : Person("John", 160)
+	{
+	}
 
-    int getGroup() const
-    {
-        return m_group_number;
-    }
+	int getGroup() const
+	{
+		return m_group_number;
+	}
 
 private:
-    int m_group_number;
+	int m_group_number;
 };
 
 class Rational
 {
 public:
-    Rational(int n = 0, unsigned d = 1) : num(n), den(d)
-    {}
+	Rational(int n = 0, unsigned d = 1) : num(n), den(d)
+	{}
 
-    void setNum(int n)
-    {
-        num = n;
-    }
+	void setNum(int n)
+	{
+		num = n;
+	}
 
-    void setDen(int d)
-    {
-        if (d == 0)
-            throw BadArgument();
-        den = d;
-    }
+	void setDen(int d)
+	{
+		if (d == 0)
+			throw BadArgument();
+		den = d;
+	}
 
-    int getNum() const
-    {
-        return num;
-    }
+	int getNum() const
+	{
+		return num;
+	}
 
-    unsigned getDen() const
-    {
-        return den;
-    }
+	unsigned getDen() const
+	{
+		return den;
+	}
 
-    Rational sum(const Rational& r)
-    {
-        Rational res;
-        res.num = num * r.den + r.num * den;
-        res.den = den * r.den;
-        return res;
-    }
+	Rational sum(const Rational& r)
+	{
+		Rational res;
+		res.num = num * r.den + r.num * den;
+		res.den = den * r.den;
+		return res;
+	}
 
 
-    Rational operator+(const Rational& r)
-    {
-        Rational res;
-        res.num = num * r.den + r.num * den;
-        res.den = den * r.den;
-        return res;
-    }
+	Rational operator+(const Rational& r)
+	{
+		Rational res;
+		res.num = num * r.den + r.num * den;
+		res.den = den * r.den;
+		return res;
+	}
 
 private:
-    int num;
-    unsigned den;
+	int num;
+	unsigned den;
 };
 
 template<typename Fun, typename Arg>
 Arg findZero(Fun function, Arg low, Arg high, Arg eps)
 {
-    Arg lowValue = function(low);
-    Arg highValue = function(high);
-    if (highValue * lowValue < 0)
-    {
-        Arg mid = (high + low) / 2;
-        Arg midValue = function(mid);
-        while (std::abs(midValue) > eps)
-        {
-            if (lowValue * midValue > 0)
-            {
-                lowValue = midValue;
-                low = mid;
-            }
-            else
-            {
-                highValue = midValue;
-                high = mid;
-            }
-            mid = (high + low) / 2;
-            midValue = function(mid);
-        }
-        return mid;
-    }
-    throw Error();
+	Arg lowValue = function(low);
+	Arg highValue = function(high);
+	if (highValue * lowValue < 0)
+	{
+		Arg mid = (high + low) / 2;
+		Arg midValue = function(mid);
+		while (std::abs(midValue) > eps)
+		{
+			if (lowValue * midValue > 0)
+			{
+				lowValue = midValue;
+				low = mid;
+			}
+			else
+			{
+				highValue = midValue;
+				high = mid;
+			}
+			mid = (high + low) / 2;
+			midValue = function(mid);
+		}
+		return mid;
+	}
+	throw Error();
 }
 
 template<typename T>
 T func1(T x)
 {
-    return (x - 3) * x * (x - 2);
+	return (x - 3) * x * (x - 2);
 }
 
 // Функция для вычисления производной в точке
 template<typename Fun, typename Arg>
 Arg derivative(Fun function, Arg x)
 {
-    double dx = 1e-5;
-    return (function(x + dx) - function(x)) / dx;
+	double dx = 1e-5;
+	return (function(x + dx) - function(x)) / dx;
 }
 
 std::ostream& operator<<(std::ostream& ost, const Rational& r)
 {
-    ost << r.getNum() << "/" << r.getDen();
-    return ost;
+	ost << r.getNum() << "/" << r.getDen();
+	return ost;
 }
 
 void printContents(Storage<double> s)
 {
-    for (size_t k = 0; k < s.size(); ++k)
-    {
-        std::cout << s[k] << std::endl;
-        s[k] = s[k] + 34.5 + 7.88;
-    }
+	for (size_t k = 0; k < s.size(); ++k)
+	{
+		std::cout << s[k] << std::endl;
+		s[k] = s[k] + 34.5 + 7.88;
+	}
 }
 
 template<typename Fun, typename Arg>
 Arg NewtMeth(Fun function, Arg low, Arg high, Arg eps)
 {
-    double x = (low + high) / 2;
-    double min = 1000000;
-    for (int i = low; i < high; i++)
-        if (min > derivative(function, i))
-        {
-            min = derivative(function, i);
-        }
-    double x0 = -1000, x1;
-    x1 = x - function(x) / derivative(function, x);
-    while (abs(function(x1) / min) > eps)
-    {
-        x0 = x1;
-        x1 = x0 - function(x0) / derivative(function, x0);
-    }
-    return (x1);
+	double x = (low + high) / 2;
+	double min = 1000000;
+	for (int i = low; i < high; i++)
+		if (min > derivative(function, i))
+		{
+			min = derivative(function, i);
+		}
+	double x0 = -1000, x1;
+	x1 = x - function(x) / derivative(function, x);
+	while (abs(function(x1) / min) > eps)
+	{
+		x0 = x1;
+		x1 = x0 - function(x0) / derivative(function, x0);
+	}
+	return (x1);
 }
 
 
 void sayHello(WithName* p)
 {
-    std::cout << "Hello " << p->getName() << std::endl;
+	std::cout << "Hello " << p->getName() << std::endl;
 }
 
 void getErr(IRequirement* req)
 {
-    std::cout << "Error now " << req->error() << std::endl;
+	std::cout << "Error now " << req->error() << std::endl;
 }
 
 #include <SFML/Graphics.hpp>
@@ -197,21 +197,21 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    Controller c;
+	setlocale(LC_ALL, "rus");
+	Controller c;
 
-    Storage<double> s;
-    s.add(0);
-    s.add(0);
-    s.add(30);
+	Storage<double> s;
+	s.add(0);
+	s.add(0);
+	s.add(30);
 
-    c.addPrimitive(P_Circle, s);
-    s.add(60);
-    c.addPrimitive(P_Segment, s);
+	c.addPrimitive(P_Circle, s);
+	s.add(60);
+	c.addPrimitive(P_Segment, s);
 
-    while (c.updateView())
-    {};//!DON NOT REMOVE!
+	while (c.updateView())
+	{};//!DON NOT REMOVE!
 
-    return 0;
+	return 0;
 
 }
