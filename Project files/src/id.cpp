2d3f23a21242df54da6f60738e1a23d1f5ee4c2b
Григорий Григорьев <nigd@bk.ru>
2020-04-07 08:16:27 +0300

    lecture + refactoring

--- Project files/src/id.cpp
+++ Project files/src/id.cpp
@@ -5,30 +5,30 @@ size_t ID::m_count = 0;
 
 ID::ID()
 {
-    ++m_count;
-    char buf[1024];
+	++m_count;
+	char buf[1024];
 
-    for (size_t k = 0; k < sizeof(buf); ++k)
-        buf[k] = 0;
+	for (size_t k = 0; k < sizeof(buf); ++k)
+		buf[k] = 0;
 
-    snprintf(buf, sizeof(buf), "%zu", m_count);
-    for (size_t i = 0; i < 1024; ++i)
-        if (buf[i] != 0)
-            m_array.add(buf[i]);
-        else
-            break;
+	snprintf(buf, sizeof(buf), "%zu", m_count);
+	for (size_t i = 0; i < 1024; ++i)
+		if (buf[i] != 0)
+			m_array.add(buf[i]);
+		else
+			break;
 }
 
 bool ID::operator==(const ID& id) const
 {
-    if (m_array.size() != id.m_array.size())
-        return false;
+	if (m_array.size() != id.m_array.size())
+		return false;
 
-    for (size_t k = 0; k < m_array.size(); ++k)
-        if (m_array[k] != id.m_array[k])
-            return false;
+	for (size_t k = 0; k < m_array.size(); ++k)
+		if (m_array[k] != id.m_array[k])
+			return false;
 
-    return true;
+	return true;
 }
 
 bool ID::operator<(const ID& id) const
@@ -38,20 +38,21 @@ bool ID::operator<(const ID& id) const
 		return false;
 	if (m_array.size() < id.m_array.size())
 		return true;
-	if (m_array.size() == id.m_array.size()) {
+	if (m_array.size() == id.m_array.size())
+	{
 		for (size_t k = 0; k < m_array.size(); ++k)
 			if (m_array[k] < id.m_array[k]) return true;
-			else if (m_array[k] > id.m_array[k]) return false;	
+			else if (m_array[k] > id.m_array[k]) return false;
 	}
 	return false;
 }
 
 void ID::initCount(size_t newCount)
 {
-    m_count = newCount;
+	m_count = newCount;
 }
 
 size_t ID::getLastCount()
 {
-    return m_count;
+	return m_count;
 }
--- Project files/src/main.cpp
+++ Project files/src/main.cpp
@@ -9,236 +9,346 @@
 class WithName
 {
 public:
-    virtual const char* getName() const = 0;
+	virtual const char* getName() const = 0;
 };
 
 class Person : public WithName
 {
 public:
-    Person(const char* name, int height) : m_name(name), m_height(height)
-    {
-    }
-    virtual const char* getName() const
-    {
-        return m_name;
-    }
-        int getHeight() const
-    {
-        return m_height;
-    }
+	Person(const char* name, int height) : m_name(name), m_height(height)
+	{
+	}
+
+	virtual const char* getName() const
+	{
+		return m_name;
+	}
+
+	int getHeight() const
+	{
+		return m_height;
+	}
 
 private:
-    const char* m_name;
-    int m_height;
+	const char* m_name;
+	int m_height;
 };
 
 class Student : public Person
 {
 public:
-    Student() : Person("John", 160)
-    {
-    }
-    int getGroup() const
-    {
-        return m_group_number;
-    }
+	Student() : Person("John", 160)
+	{
+	}
+
+	int getGroup() const
+	{
+		return m_group_number;
+	}
 
 private:
-    int m_group_number;
+	int m_group_number;
 };
 
 class Street : public WithName
 {
 public:
-    Street(char* Sname = "Botanicheskaya") :m_Sname(Sname)
-    {
-    }
-    virtual const char* getName() const
-    {
-        return m_Sname;
-    }
+	Street(char* Sname = "Botanicheskaya") : m_Sname(Sname)
+	{
+	}
+
+	virtual const char* getName() const
+	{
+		return m_Sname;
+	}
 
 private:
-    char* m_Sname;
-    int m_numHouse;
+	char* m_Sname;
+	int m_numHouse;
 };
 
 class Rational
 {
 public:
-    Rational(int n = 0, unsigned d = 1) : num(n), den(d)
-    {}
+	Rational(int n = 0, unsigned d = 1) : num(n), den(d)
+	{}
 
-    void setNum(int n)
-    {
-        num = n;
-    }
+	void setNum(int n)
+	{
+		num = n;
+	}
 
-    void setDen(int d)
-    {
-        if (d == 0)
-            throw BadArgument();
-        den = d;
-    }
+	void setDen(int d)
+	{
+		if (d == 0)
+			throw BadArgument();
+		den = d;
+	}
 
-    int getNum() const
-    {
-        return num;
-    }
+	int getNum() const
+	{
+		return num;
+	}
 
-    unsigned getDen() const
-    {
-        return den;
-    }
+	unsigned getDen() const
+	{
+		return den;
+	}
 
-    Rational sum(const Rational& r)
-    {
-        Rational res;
-        res.num = num * r.den + r.num * den;
-        res.den = den * r.den;
-        return res;
-    }
+	Rational sum(const Rational& r)
+	{
+		Rational res;
+		res.num = num * r.den + r.num * den;
+		res.den = den * r.den;
+		return res;
+	}
 
 
-    Rational operator+(const Rational& r)
-    {
-        Rational res;
-        res.num = num * r.den + r.num * den;
-        res.den = den * r.den;
-        return res;
-    }
+	Rational operator+(const Rational& r)
+	{
+		Rational res;
+		res.num = num * r.den + r.num * den;
+		res.den = den * r.den;
+		return res;
+	}
 
 private:
-    int num;
-    unsigned den;
+	int num;
+	unsigned den;
 };
 
 template<typename Fun, typename Arg>
 Arg findZero(Fun function, Arg low, Arg high, Arg eps)
 {
-    Arg lowValue = function(low);
-    Arg highValue = function(high);
-    if (highValue * lowValue < 0)
-    {
-        Arg mid = (high + low) / 2;
-        Arg midValue = function(mid);
-        while (std::abs(midValue) > eps)
-        {
-            if (lowValue * midValue > 0)
-            {
-                lowValue = midValue;
-                low = mid;
-            }
-            else
-            {
-                highValue = midValue;
-                high = mid;
-            }
-            mid = (high + low) / 2;
-            midValue = function(mid);
-        }
-        return mid;
-    }
-    throw Error();
+	Arg lowValue = function(low);
+	Arg highValue = function(high);
+	if (highValue * lowValue < 0)
+	{
+		Arg mid = (high + low) / 2;
+		Arg midValue = function(mid);
+		while (std::abs(midValue) > eps)
+		{
+			if (lowValue * midValue > 0)
+			{
+				lowValue = midValue;
+				low = mid;
+			}
+			else
+			{
+				highValue = midValue;
+				high = mid;
+			}
+			mid = (high + low) / 2;
+			midValue = function(mid);
+		}
+		return mid;
+	}
+	throw Error();
 }
 
 template<typename T>
 T func1(T x)
 {
-    return (x - 3) * x * (x - 2);
+	return (x - 3) * x * (x - 2);
 }
 
 // Функция для вычисления производной в точке
-template <typename Fun, typename Arg>
+template<typename Fun, typename Arg>
 Arg derivative(Fun function, Arg x)
 {
-    double dx = 1e-5;
-    return (function(x + dx) - function(x)) / dx;
+	double dx = 1e-5;
+	return (function(x + dx) - function(x)) / dx;
 }
 
 std::ostream& operator<<(std::ostream& ost, const Rational& r)
 {
-    ost << r.getNum() << "/" << r.getDen();
-    return ost;
+	ost << r.getNum() << "/" << r.getDen();
+	return ost;
 }
 
 void printContents(Storage<double> s)
 {
-    for (size_t k = 0; k < s.size(); ++k)
-    {
-        std::cout << s[k] << std::endl;
-        s[k] = s[k] + 34.5 + 7.88;
-    }
+	for (size_t k = 0; k < s.size(); ++k)
+	{
+		std::cout << s[k] << std::endl;
+		s[k] = s[k] + 34.5 + 7.88;
+	}
 }
 
 template<typename Fun, typename Arg>
 Arg NewtMeth(Fun function, Arg low, Arg high, Arg eps)
-{   
-        double x = (low + high) / 2; 
-        double  min = 1000000;
-        for (int i = low; i < high; i++)
-                if (min > derivative(function,i))
-                {
-                      min = derivative(function,i);
-                }
-        double  x0 = -1000, x1;
-	x1 = x - function(x) / derivative(function,x);
-        while (abs(function(x1) / min) > eps)
-        {
-                  x0 = x1;
-                        x1 = x0 - function(x0) / derivative(function,x0);
-        }
-        return(x1);
+{
+	double x = (low + high) / 2;
+	double min = 1000000;
+	for (int i = low; i < high; i++)
+		if (min > derivative(function, i))
+		{
+			min = derivative(function, i);
+		}
+	double x0 = -1000, x1;
+	x1 = x - function(x) / derivative(function, x);
+	while (abs(function(x1) / min) > eps)
+	{
+		x0 = x1;
+		x1 = x0 - function(x0) / derivative(function, x0);
+	}
+	return x1;
 }
 
 
 void sayHello(WithName* p)
 {
-    std::cout << "Hello " << p->getName() << std::endl;
+	std::cout << "Hello " << p->getName() << std::endl;
 }
 
 void getErr(IRequirement* req)
 {
-    std::cout << "Error now " << req->error() << std::endl;
+	std::cout << "Error now " << req->error() << std::endl;
 }
+
+Segment s;
+
+double verticalError(double x)
+{
+	Point p1 = s.getStart();
+	p1.setX(x);
+	Point p2 = s.getEnd();
+
+	//s = Segment(&p1, &p2);
+
+	double err = x - p2.getX();
+	return err;
+}
+
+class verticalErrorType
+{
+public:
+	verticalErrorType(Segment* segment = nullptr) : m_segment(segment)
+	{
+	}
+
+	Segment* getSegmentPointer() const
+	{
+		return m_segment;
+	}
+
+	double operator()(double x)
+	{
+		Point p2 = m_segment->getEnd();
+		return x - p2.getX();
+	}
+
+private:
+	Segment* m_segment;
+};
+
+/*
+class Totals
+{
+public:
+	Totals(const Storage<verticalErrorType>& storage) : m_verticalErrs(storage)
+	{
+	}
+
+	double operator()(double x)
+	{
+		double res = 0;
+		Storage<verticalErrorType>::Marker marker = m_verticalErrs.createMarker();
+		for (; marker.hasNext(); marker.next())
+		{
+			res += marker.getValue()(x);
+		}
+	}
+
+private:
+	Storage<verticalErrorType> m_verticalErrs;
+};
+*/
 
 int main()
 {
-    try
-    {
-        Person p("Mary", 163);
-        Student s;
-        Street st;
-        sayHello(&p);
-        sayHello(&s);
-        sayHello(&st);
+	try
+	{
+		double x1 = 0, y1 = 0, x2 = 1, y2 = 1;
+		Point a1;
+		a1.setX(x1);
+		a1.setY(y1);
 
-        std::cout << "Student's name is " << s.getName()<< std::endl;
-        std::cout << "Student's height is " << s.getHeight() << std::endl;
+		Point a2;
+		a2.setX(x2);
+		a2.setY(y2);
 
-        Point p1, p2(1,1), p3(1,0), p4(2,1);
-        Segment se(&p3, &p4);
-        HorizontalRequirement hr(&se);
-        PointOnSegmentRequirement pos(&p1, &se);
+		Point a3;
+		a3.setX(6);
+		a3.setY(6);
 
-        std::cout << hr.error() << std::endl;
-        std::cout << pos.error() << std::endl;
+		s = Segment(&a1, &a2);
 
-        /*
-        IRequirement* ptrreq = (IRequirement*)&hr;
-        std::cout << ptrreq->error() << std::endl;
-        ptrreq = (IRequirement*)&pos;
-        std::cout << ptrreq->error() << std::endl;
-        */
+		Segment s2(&a1, &a2);
 
-        getErr((IRequirement*) &hr);
-        getErr((IRequirement*) &pos);
-        
-        std::cout << findZero(func1<double>, -1.0, 1.0, 0.00001) << std::endl;
-        std::cout << findZero(func1<double>, 1.0, 2.5, 0.0000001) << std::endl;
-        std::cout << derivative(func1<double>, 8.0) << std::endl;
-        std::cout << NewtMeth(func1<double>, 1.5, 2.5, 0.0000001) << std::endl;
-        MMap<int, double> mmap;
+		verticalErrorType vt(&s2);
+
+		Segment s3(&a2, &a3);
+
+		verticalErrorType vt1(&s3);
+
+		//Storage<verticalErrorType> storage;
+		//storage.add(vt);
+		//storage.add(vt1);
+
+		//Totals tot(storage);
+		//double res = findZero(tot, -2.0, 2.0, 0.00001);
+
+		//std::cout << res << std::endl;
+
+		//double res = findZero(vertError, -2.0, 2.0, 0.00001);
+		double res = findZero(vt, -2.0, 2.0, 0.00001);
+
+		std::cout << "res = " << res << " vt1(res) = " << vt1(1) << std::endl;
+
+		std::cout << res << std::endl;
+		a1.setX(res);
+
+		res = findZero(vt1, -2.0, 20.0, 0.00001);
+
+		a2.setX(res);
+
+		std::cout << vt1(1) << std::endl;
+
+
+		Person p("Mary", 163);
+		Student stu;
+		Street st;
+		sayHello(&p);
+		sayHello(&stu);
+		sayHello(&st);
+
+		std::cout << "Student's name is " << stu.getName() << std::endl;
+		std::cout << "Student's height is " << stu.getHeight() << std::endl;
+
+		Point p1, p2(1, 1), p3(1, 0), p4(2, 1);
+		Segment se(&p3, &p4);
+		HorizontalRequirement hr(&se);
+		PointOnSegmentRequirement pos(&p1, &se);
+
+		std::cout << hr.error() << std::endl;
+		std::cout << pos.error() << std::endl;
+
+		/*
+		IRequirement* ptrreq = (IRequirement*)&hr;
+		std::cout << ptrreq->error() << std::endl;
+		ptrreq = (IRequirement*)&pos;
+		std::cout << ptrreq->error() << std::endl;
+		*/
+
+		getErr((IRequirement*) &hr);
+		getErr((IRequirement*) &pos);
+
+		std::cout << findZero(func1<double>, -1.0, 1.0, 0.00001) << std::endl;
+		std::cout << findZero(func1<double>, 1.0, 2.5, 0.0000001) << std::endl;
+		std::cout << derivative(func1<double>, 8.0) << std::endl;
+		std::cout << NewtMeth(func1<double>, 1.5, 2.5, 0.0000001) << std::endl;
+		MMap<int, double> mmap;
 //        mmap.add();
 
 //        Controller controller;
@@ -265,10 +375,10 @@ int main()
 //
 //        controller.updateView();
 
-    } catch (...)
-    {
-        std::cout << "Whoops!" << std::endl;
-    }
+	} catch (...)
+	{
+		std::cout << "Whoops!" << std::endl;
+	}
 
-    return 0;
+	return 0;
 }
