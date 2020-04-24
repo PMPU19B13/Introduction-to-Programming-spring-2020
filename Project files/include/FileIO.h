90a3c2237b5c4c6797dd49dc5bc8a32d24fe4f0d
Dmitry913 <55257488+Dmitry913@users.noreply.github.com>
2020-04-24 16:16:18 +0700

    Update FileIO.h

--- Project files/include/FileIO.h
+++ Project files/include/FileIO.h
@@ -2,17 +2,19 @@
 
 #include <string>
 #include <fstream>
-
 #include "controller.h"
+
+class Controller;
+
+enum PrimitiveType;
 
 class FileIO
 {
 private:
 
 public:
-	static void readPrimitive(const std::string& filename, Controller& controller);
-	static void writePrimitive(const std::string& filename, Controller& controller);
-	static void writeRequirement(const std::string& filename, Controller& controller);
-	static void readRequirement(const std::string& filename, Controller& controller);
-};
 
+	static void read(const std::string& filename, Controller& controller);
+
+	static void write(const std::string& filename, PrimitiveType type, const Pair<ID, Storage<double>>& params);
+};
